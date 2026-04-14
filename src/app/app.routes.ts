de72cdb07b98474b1d3b41c9c1204681355aeffb
adan <ac7592422@gmail.com>
2026-04-13 20:21:19 -0700

    se agrego menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { NegocioComponent } from './Negocio/Negocio.component';
 import { OfertaComponent } from './Oferta/Oferta.component';
 import { Usuario_NegocioComponent } from './UsuarioNegocio/Usuario_Negocio.component';
+import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
-  { path: 'negocios', component: NegocioComponent },
-  { path: 'ofertas', component: OfertaComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'negocio', component: NegocioComponent },
+  { path: 'oferta', component: OfertaComponent },
   { path: 'usuario-negocio', component: Usuario_NegocioComponent },
-  { path: '', redirectTo: '/negocios', pathMatch: 'full' }
+  { path: '', redirectTo: 'menu', pathMatch: 'full' }
 ];
